de586efda5c425547a857e9a981128617cdc8f10
TyHafen <tyhafen@gmail.com>
2022-05-04 15:27:12 -0600

    Data models

--- SpotCheck/dbSetup.sql
+++ SpotCheck/dbSetup.sql
@@ -8,3 +8,16 @@ CREATE TABLE IF NOT EXISTS accounts(
 ) default charset utf8 COMMENT '';
 
 SELECT * FROM accounts;
+
+CREATE TABLE IF NOT EXISTS spots(
+	id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+	updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+	name VARCHAR(255) COMMENT "Spot Name",
+	address VARCHAR(255) COMMENT "Spot Address",
+	price TEXT NOT NULL,
+	creatorId VARCHAR(255) NOT NULL,
+	FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+
+SELECT * FROM spots;
